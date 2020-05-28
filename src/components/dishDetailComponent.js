023c0c1633993b4cf30b3982c8efb1f35cd2443b
Ganesh-11062000 <ganeshnm2000@gmail.com>
2020-05-28 11:34:05 +0530

    Single Page Application Part-2

--- src/components/dishDetailComponent.js
+++ src/components/dishDetailComponent.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
-import {Card,CardBody,CardText,CardImg,CardTitle,} from 'reactstrap';
+import {Card,CardBody,CardText,CardImg,CardTitle, Breadcrumb, BreadcrumbItem,} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 class DishDetail extends Component{
 
@@ -8,7 +9,7 @@ class DishDetail extends Component{
             <Card>
                 <CardImg src={dish.image} alt={dish.name}></CardImg>
                 <CardBody>
-        <CardTitle>{dish.name}</CardTitle>
+                    <CardTitle>{dish.name}</CardTitle>
                     <CardText>
                         {dish.description}
                     </CardText>
@@ -17,12 +18,10 @@ class DishDetail extends Component{
         );
     }
 
-    renderComments(dish){ 
+    renderComments(comments){ 
         return(
-          <div className="">
-              {dish.comments.map((comment) => {
-
-                console.log("comment ")
+          <div>
+              {comments.map((comment) => {
 
                 return <div key={comment.id}>
                 <p>{comment.comment}</p>
@@ -37,13 +36,21 @@ class DishDetail extends Component{
         if(this.props.dish != null){
             return(
                 <div className="row">
+                    <div className="col-12">
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to="/menu">Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                    </div>
                     <div className="col-12 col-md-5 m-1">
                         {this.renderDish(this.props.dish)}
                     </div>
                     <div className="col-12 col-md-5 m-1">
                         <h4>Comments</h4>
                         <ul className="list-unstyled">
-                        {this.renderComments(this.props.dish)}
+                        {this.renderComments(this.props.comments)}
                         </ul>
                     </div>
                 </div>
